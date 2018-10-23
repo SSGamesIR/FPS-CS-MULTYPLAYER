d0c6b5ac05174551d91a721380bad1b7be49c6a6
stonecoold <saadati.shahriar@hotmail.com>
2018-10-23 07:33:06 +0330

    all the project files plus a test build
    for more question contact me
    it can be a little hard to run for the first time

---
+++ Assets/Scripts/FPS Character Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerHealth : NetworkBehaviour {
+
+    [SyncVar]
+    public float health = 100f;
+
+
+    public void TakeDamage(float damage)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        health -= damage;
+        print("Damage khordam & jonam is now " + health);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+
+
+}//class
